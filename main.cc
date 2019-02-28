bba2eca96d9840d81ae6d4adf093e93388a54356
Gustav <sir.gustav.the.coder@gmail.com>
2019-02-28 06:42:02 +0100

    basic key input

--- main.cc
+++ main.cc
@@ -5,6 +5,10 @@ struct Screen
   Screen()
   {
     initscr();
+
+    raw(); // disable linebuffer
+    keypad(stdscr, TRUE); // ket f1, f2 etc...
+    noecho(); // getch doesn't echo
   }
 
   ~Screen()
@@ -13,13 +17,39 @@ struct Screen
   }
 };
 
+struct Bold
+{
+  Bold()
+  {
+    attron(A_BOLD);
+  }
+  ~Bold()
+  {
+    attroff(A_BOLD);
+  }
+};
+
 int main()
 {	
   Screen screen;
 
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
+	printw("Type any key to see it in bold\n");
+	refresh();
+
+  const auto ch = getch();
+  if(ch == KEY_F(1))
+  {
+    printw("f1 pressed");
+  }
+  else
+  {
+    printw("Key pressed was (%d): ", ch);
+    Bold bold;
+    printw("%c", ch);
+  }
+  refresh();
+
+	getch();
 
 	return 0;
 }
